3be5e1257fea9df94c05bb0a137799678044b2c5
hpatel95 <61155458+hpatel95@users.noreply.github.com>
2020-02-20 10:07:08 +0100

    Update compare_multiple_group.R

--- barplots/multiple groups/script/compare_multiple_group.R
+++ barplots/multiple groups/script/compare_multiple_group.R
@@ -19,3 +19,30 @@ gstack <- moyenne_desf_sev_par_annee %>%
   ggplot(aes(x = annee, y = Moyenne, fill = Gaz)) + 
   geom_bar(stat="identity") 
 ```
+
+#########################
+#Variables qualitatives (classes d'IMC ici), en fonction d'une autre variable quantitative (moyenne de desflurane ici)
+
+##Barplot
+
+
+```{r Graph à barre2, echo=TRUE}
+
+g2barre <- hal_data %>% 
+  group_by (imc_classe) %>% 
+  summarise(moyenne_desf = mean(moy_dese_anesth, na.rm = TRUE)) %>% #(na.rm Pas compter les NAs)
+  filter(!imc_classe %in% c("")) %>%
+  ggplot( aes(x=imc_classe, y=moyenne_desf)) +
+  geom_bar(stat="identity")
+
+##Interactiv barplot
+```{r Graph à barre2 plotly, echo=TRUE}
+
+p2barre <- hal_data %>% 
+  group_by (imc_classe) %>% 
+  summarise(moyenne_desf = mean(moy_dese_anesth, na.rm = TRUE)) %>% #(na.rm Pas compter les NAs)
+  filter(!imc_classe %in% c("")) %>%
+  plot_ly(x = ~imc_classe,
+          y = ~moyenne_desf,
+            name = "Moyenne desf/classe_imc",
+          type = "bar")
